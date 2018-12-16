1a4644215e3bc3a0dc99021a006065b0ee1e7f03
Your Name <mann_9898771157@yahoo.com>
2018-12-16 18:51:23 +0530

    new file added

---
+++ MMasterDemo/Controller/TableViewWithAVC.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewWithAVC.swift
+//  MMasterDemo
+//
+//  Created by Manish Sharma on 16/12/18.
+//  Copyright © 2018 Manish Sharma. All rights reserved.
+//
+
+import UIKit
+
+class TableViewWithAVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
